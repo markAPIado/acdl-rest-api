e5015b33ee4f5d85b6b707091b38276915ba4c9f
Mark Apiado <apiadomptc@gmail.com>
2023-11-10 07:15:53 +0800

    refactor: add return type

--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -1,23 +1,29 @@
-import { HydratedDocument } from 'mongoose';
-import { DeleteProductDto, ProductDto, UpdateProductDto } from './product.dto';
-import Product from './product.model';
+import { HydratedDocument, Query } from 'mongoose';
+import {
+  DeleteProductDto,
+  CreateProductDto,
+  UpdateProductDto
+} from './product.dto';
+import Product, { IProduct } from './product.model';
 
-export function createProduct(input: ProductDto): HydratedDocument<ProductDto> {
+export function createProduct(
+  input: CreateProductDto['body']
+): HydratedDocument<IProduct> {
   return new Product(input);
 }
 
-export function getProductById(id: string) {
+export function getProductById(id: string): Query<IProduct | null, IProduct> {
   return Product.findById(id);
 }
 
-export function getProductList() {
+export function getProductList(): Query<IProduct[], IProduct> {
   return Product.find();
 }
 
 export function updateProduct(
   id: UpdateProductDto['params']['id'],
   product: UpdateProductDto['body']
-) {
+): Query<IProduct | null, IProduct> {
   return Product.findByIdAndUpdate(id, product, { new: true });
 }
 
